df7b38fe35abf76312bac6d20015e03c14846c7b
Khavol <olehkhavar@gmail.com>
2024-09-29 20:43:40 +0300

    add tests for method

--- CryptoTrackerTests/CryptoCapitalizationCalculatorTest.cs
+++ CryptoTrackerTests/CryptoCapitalizationCalculatorTest.cs
@@ -1,15 +1,61 @@
+using CryptoTracker.Services;
+
 namespace CryptoTrackerTests;
 
 public class CryptoCapitalizationCalculatorTest
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void CalculateMarketCap_ValidPriceAndSupply_ReturnsCorrectMarketCap()
     {
+        decimal? price = 50000m;
+        decimal? circulatingSupply = 1000000m;
+        
+        var result = CryptoCapitalizationCalculator.CalculateMarketCap(price, circulatingSupply);
+        
+        Assert.That(result, Is.EqualTo(50000000000m));
     }
 
     [Test]
-    public void Test1()
+    public void CalculateMarketCap_NullPrice_ReturnsNull()
     {
-        Assert.Pass();
+        decimal? price = null;
+        decimal? circulatingSupply = 1000000m;
+        
+        var result = CryptoCapitalizationCalculator.CalculateMarketCap(price, circulatingSupply);
+        
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public void CalculateMarketCap_NullCirculatingSupply_ReturnsNull()
+    {
+        decimal? price = 50000m;
+        decimal? circulatingSupply = null;
+        
+        var result = CryptoCapitalizationCalculator.CalculateMarketCap(price, circulatingSupply);
+        
+        Assert.IsNull(result);
+    }
+
+    [Test]
+    public void CalculateMarketCap_ZeroPriceOrSupply_ReturnsZero()
+    {
+        decimal? price = 0m;
+        decimal? circulatingSupply = 1000000m;
+
+        var result = CryptoCapitalizationCalculator.CalculateMarketCap(price, circulatingSupply);
+
+        Assert.That(result, Is.EqualTo(0m));
+    }
+
+    [Test]
+    public void CalculateMarketCap_ZeroCirculatingSupply_ReturnsZero()
+    {
+        decimal? price = 50000m;
+        decimal? circulatingSupply = 0m;
+
+        var result = CryptoCapitalizationCalculator.CalculateMarketCap(price, circulatingSupply);
+
+        Assert.That(result, Is.EqualTo(0m));
     }
 }
