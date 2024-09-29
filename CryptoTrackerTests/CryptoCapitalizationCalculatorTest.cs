fe7705f467545da27b1d1e7757c06233616774c7
Khavol <olehkhavar@gmail.com>
2024-09-29 20:18:09 +0300

    add project with tests

---
+++ CryptoTrackerTests/CryptoCapitalizationCalculatorTest.cs
@@ -0,0 +1,15 @@
+namespace CryptoTrackerTests;
+
+public class CryptoCapitalizationCalculatorTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
